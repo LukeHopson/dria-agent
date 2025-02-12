76863895833a80431a7f85df2347558f58a959d8
andthattoo <omer@firstbatch.xyz>
2025-02-12 20:32:28 +0300

    math examples comment removed

--- examples/math.py
+++ examples/math.py
@@ -7,7 +7,7 @@ from dria_agent.tools.library.math_tools import MATH_TOOLS
 agent = ToolCallingAgentFactory.create(tools=MATH_TOOLS, backend="ollama")
 
 console = Console()
-"""
+
 # --- Example 1: Simple tool usage ---
 query = "Calculate the area of a triangle with 3 sides: a = 3 and b = 4 and c = 5"
 execution = agent.run(query)
@@ -26,7 +26,6 @@ panel = Panel(
     expand=False,
 )
 console.print(panel)
-"""
 
 # --- Example 3: Use single tools for multiple task ---
 query = "Please solve 5x^2 + 8x + 9 = 0 and 4x^2 + 11x - 3 = 0"
